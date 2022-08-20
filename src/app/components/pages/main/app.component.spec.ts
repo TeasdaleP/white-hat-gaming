b4d09d4c1d80b11aaed1f1df4efb6804308bff04
TeasdaleP <pteasdale55@gmail.com>
2022-08-20 10:02:46 +0100

    amending unit tests for main.component and including them for services

--- src/app/components/pages/main/app.component.spec.ts
+++ src/app/components/pages/main/app.component.spec.ts
@@ -1,22 +1,31 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: MockStore;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      providers: [
+        provideMockStore({})
+      ]
     }).compileComponents();
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    store = TestBed.inject(MockStore);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Should create', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Game } from "../ngrx/games/games.model";
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class GamesService {
 
     constructor(private http: HttpClient) { }
 
-    getGames$(): Observable<Array<any>> {
-        return this.http.get<Array<any>>(`${environment.baseUrl}/games.php`);
+    getGames$(): Observable<Array<Game>> {
+        return this.http.get<Array<Game>>(`${environment.baseUrl}/games.php`);
     }
 }
