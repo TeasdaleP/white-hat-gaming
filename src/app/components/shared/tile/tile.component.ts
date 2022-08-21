16a9ee7e9a351dd25108e7072da753961392818a
TeasdaleP <pteasdale55@gmail.com>
2022-08-21 09:58:18 +0100

    ribbon for new and top

--- src/app/components/shared/tile/tile.component.ts
+++ src/app/components/shared/tile/tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Ribbon } from "src/app/helpers/ribbon.enum";
 import { Game } from "src/app/ngrx/games";
 
 @Component({
@@ -8,10 +9,23 @@ import { Game } from "src/app/ngrx/games";
 })
 export class TileComponent implements OnInit {
     @Input() public game!: Game | null;
+    public ribbon: string | undefined
 
     constructor() {}
 
     ngOnInit(): void {
-        
+        if(this.needRibbon()) {
+            if(this.game?.categories.includes(Ribbon.Top) && this.game.categories.includes(Ribbon.New)) {
+                this.ribbon = `${Ribbon.Top} & ${Ribbon.New}`;
+            } else if (this.game?.categories.includes(Ribbon.Top)) {
+                this.ribbon = Ribbon.Top;
+            } else if (this.game?.categories.includes(Ribbon.New)) {
+                this.ribbon = Ribbon.New;
+            } 
+        }
+    }
+
+    public needRibbon(): boolean | undefined {
+        return this.game?.categories.includes(Ribbon.Top) || this.game?.categories.includes(Ribbon.New);
     }
 }
